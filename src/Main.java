7f8d86ef751115ced61b3615a6c022467dde0dd2
Gavriil Fursov <from.skveki1@mail.ru>
2022-09-07 00:41:48 +0300

    Adding lambda

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -9,7 +10,19 @@ public class Main {
         persons.add(new Person("Markus", "Berg ", 28));
         persons.add(new Person("Marshmallow", "Legend Ice", 20));
         persons.add(new Person("Andy", "Corner Jey Rodriguez", 18));
-        Collections.sort(persons, new TwoSurname(2));
+        Comparator<Person> personComparator = (o1, o2) -> {
+            int maxLength = 0;
+            String[] a1 = o1.getSurname().split(" ", maxLength);
+            String[] a2 = o2.getSurname().split(" ", maxLength);
+            if (a1.length > a2.length) {
+                return -1;
+            } else if (a1.length < a2.length) {
+                return 1;
+            } else {
+                return Integer.compare(o2.getAge(), o1.getAge());
+            }
+        };
+        Collections.sort(persons, personComparator);
         for (Person p : persons) {
             System.out.println(p);
         }
